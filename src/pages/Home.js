2b7f8604db934c97f962d9c9c768d19ff4314ea1
Carlstong <carlstongrefalde@gmail.com>
2024-02-27 16:03:20 +0800

    brgy docx

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
           <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:place-items-center md:gap-10 lg:gap-20">
             <article>
               <h1 className="text-slate-700 mb-5 text-4xl md:text-5xl font-bold text-center md:text-left">
-                Create invoices with Invoicer
+              Barangay Invoicer and Document Records
               </h1>
               <article className="md:hidden">
                 <img
